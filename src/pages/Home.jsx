d04646978fad08f2b9957148374977d900394b7d
Emmakin <101441304+Emmakin@users.noreply.github.com>
2022-10-05 23:36:09 +0100

    Update

---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+function Home() {
+  return (
+    <div>
+      <h1 style={style.title}>
+        Introducing <br />
+        Nike Forward
+      </h1>
+      <div style={style.subtitle}>
+        Made with an average 75% less carbon when compare to our traditional <br />
+        knee fleece. Drapes, feel and wears like nothing you have experience before.
+      </div>
+
+    </div>
+  );
+}
+
+const style = {
+  title: {
+    fontFamily: "Nike Futura",
+    textTransform: "uppercase",
+    fontSize: "72px",
+    lineHeight: "60px",
+    textAlign: "center",
+    margin: "0 auto",
+  },
+  subtitle: {
+    fontFamily: "Nike Futura",
+    textAlign: "center",
+    marginTop: "2rem"
+  }
+};
+
+export default Home;
