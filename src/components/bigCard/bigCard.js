f701cca56f68bba6f2c15ed6d499431641fb53e4
Emmakin <101441304+Emmakin@users.noreply.github.com>
2022-10-11 21:44:36 +0100

    Update

--- src/components/bigCard/bigCard.js
+++ src/components/bigCard/bigCard.js
@@ -4,7 +4,7 @@ import Styles from "./bigCard.module.css";
 function Imagediv({src, alt, text, button}) {
   return (
     <div  className={Styles.mainPic}>
-        <img src={src} alt={alt} />
+        <img src={src} alt={alt}  />
         <div  className={Styles.text}>{text}</div>
         <button  className={Styles.btn}>{button}</button>
     </div>
