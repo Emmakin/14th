202b7caa8686b3b0bee8638e7540e481783cdd81
Emmakin <101441304+Emmakin@users.noreply.github.com>
2022-10-16 22:56:05 +0100

    Update section4.js

--- src/components/sections/section4/section4.js
+++ src/components/sections/section4/section4.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import Styles from './secton4'
+import Styles from './secton4.module.css'
 
 export const Section4 = () => {
   return (
-    <div>
+    <div className={Styles.wrapper}>
 
     </div>
   )
