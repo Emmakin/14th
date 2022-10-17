4db704ef255b76c6a72065a28ae73fa7a9a31cec
Emmakin <101441304+Emmakin@users.noreply.github.com>
2022-10-17 23:59:24 +0100

    Update section3.js

--- src/components/sections/section3/section3.js
+++ src/components/sections/section3/section3.js
@@ -15,11 +15,11 @@ export const Section3 = () => {
       </div>
       <div>
         <ul>
-          <li>Featured</li>
-          <li><a>AirForce 1</a></li>
-          <li><a>Jordan 1</a></li>
-          <li><a>Air Max 2090</a></li>
-         <li><a>Air Max 201</a></li>
+          <li>Shoes</li>
+          <li><a>All Shoes</a></li>
+          <li><a>Jordan Shoes</a></li>
+          <li><a>Running Shoes</a></li>
+         <li><a>Basketball Shoes</a></li>
         </ul>
       </div>
       <div>
