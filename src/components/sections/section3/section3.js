adb00dcada55dc73029a0f772b9d881110bf661c
Emmakin <101441304+Emmakin@users.noreply.github.com>
2022-10-17 23:51:02 +0100

    Update

--- src/components/sections/section3/section3.js
+++ src/components/sections/section3/section3.js
@@ -2,17 +2,35 @@ import React from 'react'
 
 export const Section3 = () => {
   return (
-    <div>
+    <div className={Styles.wrapper}>
+      <div>
         <ul>
-          <li>I love my React</li>
-          <li>I love my React</li>
-          <li>I love my React</li>
-          <li>I love my React</li>
-          <li>I love my React</li>
-          <li>I love my React</li>
-          <li>I love my React</li>
-          <li>I love my React</li>
+          <li>Featured</li>
+          <li><a>AirForce 1</a></li>
+          <li><a>Jordan 1</a></li>
+          <li><a>Air Max 2090</a></li>
+         <li><a>Air Max 201</a></li>
         </ul>
+      </div>
+      <div>
+        <ul>
+          <li>Featured</li>
+          <li><a>AirForce 1</a></li>
+          <li><a>Jordan 1</a></li>
+          <li><a>Air Max 2090</a></li>
+         <li><a>Air Max 201</a></li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>Featured</li>
+          <li><a>AirForce 1</a></li>
+          <li><a>Jordan 1</a></li>
+          <li><a>Air Max 2090</a></li>
+         <li><a>Air Max 201</a></li>
+        </ul>
+      </div>
+      
     </div>
   )
 }
