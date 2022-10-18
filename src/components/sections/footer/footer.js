1bff18335d39c4ba9fed355ec460600c966bd628
Emmakin <101441304+Emmakin@users.noreply.github.com>
2022-10-18 22:41:55 +0100

    update

--- src/components/sections/footer/footer.js
+++ src/components/sections/footer/footer.js
@@ -4,7 +4,16 @@ import Styles from './footer.module.css'
 export const Footer = () => {
   return (
     <div className={Styles.footer}>
-
+        <ul className={Styles.ul_caps}>
+            <li><a>gifts card</a></li>
+            <li><a>promotions</a></li>
+            <li><a>find a store</a></li>
+            <li><a>sign up a email</a></li>
+            <li><a>become a member</a></li>
+            <li><a>nike journal</a></li>
+            <li><a>nike journal</a></li>
+            <li><a>send up feedback</a></li>
+        </ul>
     </div>
   )
 }
