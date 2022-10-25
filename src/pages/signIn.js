0ec448fe2677a14392e7d76c6e15d50c60613d12
Emmakin <101441304+Emmakin@users.noreply.github.com>
2022-10-25 23:20:05 +0100

    Update signIn.js

--- src/pages/signIn.js
+++ src/pages/signIn.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const SignIn = () => {
   return (
     <form>
-      <label></label>
+      <label>Enter your email to join us or sign in</label>
     </form>
   )
 }
