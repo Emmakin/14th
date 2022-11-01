4039595ad422e7b46f0ea4d5f3da9bbbd0833d95
Emmakin <101441304+Emmakin@users.noreply.github.com>
2022-11-01 23:45:43 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import Home from "./pages/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NotFound from "./components/pages/NotFound";
-import Shop from "./pages/Shop";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Home />;
 }
 
+
 export default App;
+
