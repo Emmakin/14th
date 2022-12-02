22129601a77400c330e80a1baa978cefa69baf3d
Emmakin <101441304+Emmakin@users.noreply.github.com>
2022-12-02 13:58:15 +0100

    update

--- src/components/shopComponent/itemCard.js
+++ src/components/shopComponent/itemCard.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import Style from './itemCard.module.css'
 
 const itemCard = () => {
     return (
         <div>
             <img src={props.img} />
-            <div>{props.}</div>
-            <div>{props.title}</div>
-            <div>{props.title}</div>
-            <div>{props.title}</div>
+            <div className={Style.name}>{props.name}</div>
+            <div>{props.price}</div>
+            <div>{props.category}</div>
+            <div>{props.tag}</div>
+            <button>Add to Cart</button>
         </div>
     );
 };
